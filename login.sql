141f85209417205a1603f44a47ecc386739866e0
Ryan <datmanwich@gmail.com>
2018-03-24 17:10:44 -0700

    posting page functionality implemented

--- login.sql
+++ login.sql
@@ -6,3 +6,15 @@ CREATE TABLE Login
 );
 
 INSERT INTO Login (id, userName, passName) VALUES (Null, 'Ryan', '1234');
+
+CREATE TABLE Posting
+(
+    postID          INT(6)          UNSIGNED AUTO_INCREMENT PRIMARY KEY
+    ,title          VARCHAR(50)     NOT NULL
+    ,specLoc        VARCHAR(100)
+    ,postCode       VARCHAR(10)
+    ,body           MEDIUMTEXT      NOT NULL
+    ,contactOp      VARCHAR(15)
+    ,phoneNum       VARCHAR(15)
+    ,contactName    VARCHAR(30)
+);
---
+++ postScript.php
@@ -0,0 +1,48 @@
+<?php
+require_once('mysqli_connect.php');
+
+    if(isset($_POST['submit'])) {
+        $title = $_POST['title'];
+        $specLoc = $_POST['specLoc'];
+        $postal = $_POST['postal'];
+        $phoneOp = $_POST['phoneOp'];
+        $phoneNum = $_POST['phone'];
+        $contact = $_POST['contact'];
+        $body = $_POST['Text1'];
+   
+        $list = connect();
+        
+        $sql = "INSERT INTO Posting (postID, title, specLoc, postCode, body, contactOp, phoneNum, contactName)
+        VALUES (NULL,'$title', '$specLoc', '$postal', '$body', '$phoneOp', '$phoneNum', '$contact')";
+        
+        sqlCheck($list, $sql);
+        
+        // Closing the connection to the database
+        mysqli_close($list);
+        die();
+    }
+
+// Connects to the database
+function connect(){
+    // Connecting to the database
+    $list = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+    // If we don't connect to the database it will spit out an error for us to fix
+    if(!$list) {
+        die("Connection failed: ".mysqli_connect_error()); // Remove the connect_error method after done testing because of hacking issues.
+    } else {
+        return $list;
+    }
+}
+
+// Checking to see if we actually placed the data into the database
+function sqlCheck($list, $sql){
+    if (!(mysqli_query($list, $sql))) {
+        echo "Error: " . $sql . "<br>" . mysqli_error($list). "<br>";
+        mysqli_close($list);
+        die();
+    } else {
+        echo "We added it to the database!";
+    }
+}
+?>
